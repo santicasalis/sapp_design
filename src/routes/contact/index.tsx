51861660cd2d60476bcda8ae8cf28c5bbfaa2386
santicasalis <santicasalis@hotmail.com>
2024-03-05 14:11:45 -0300

    form font

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -42,16 +42,16 @@ export default component$(() => {
     <>
       <h2 class="font-madimi-one">Dejame tu consulta</h2>
 
-      <div class="flex flex-col justify-center sm:flex-row">
+      <div class="mt-10 flex flex-col justify-center sm:flex-row">
         <Form
           onSubmit$={handleSubmit}
-          class="my-2 flex flex-1 flex-col  items-center"
+          class="my-2 flex flex-1 flex-col  items-center "
         >
           <Field name="name">
             {(field, props) => (
               <div>
                 <input
-                  class=" my-4 w-80 border border-solid border-gray-300 bg-slate-700 p-4  text-white "
+                  class=" font-madimi-one my-4 w-80 border border-solid border-gray-300 bg-slate-700  p-4  text-white"
                   {...props}
                   type="text"
                   value={field.value}
@@ -68,7 +68,7 @@ export default component$(() => {
                   {...props}
                   type="email"
                   value={field.value}
-                  class="my-4 w-80 border border-solid border-gray-300 bg-slate-700 p-4 text-white  "
+                  class="font-madimi-one my-4 w-80 border border-solid border-gray-300 bg-slate-700 p-4  text-white"
                   placeholder="Correo electrónico"
                 />
                 {field.error && <div class="text-red-500">{field.error}</div>}
@@ -81,7 +81,7 @@ export default component$(() => {
                 <textarea
                   {...props}
                   value={field.value}
-                  class="h-55 my-4  w-80 border border-solid border-gray-300 bg-slate-700 p-4 text-white"
+                  class="h-55 font-madimi-one  my-4 w-80 border border-solid border-gray-300 bg-slate-700 p-4 text-white"
                   placeholder="Mensaje"
                 />
                 {field.error && <div class="text-red-500">{field.error}</div>}
@@ -91,7 +91,7 @@ export default component$(() => {
           <div class="flex justify-center">
             <button
               type="submit"
-              class=" mx-auto inline-block items-center rounded-lg border-2 border-gray-700 bg-slate-700 px-8  py-3 text-white transition  duration-300 ease-in-out hover:bg-slate-400"
+              class="font-madimi-one mx-auto inline-block items-center rounded-lg border-2 border-gray-700 bg-slate-700 px-8  py-3 text-white transition  duration-300 ease-in-out hover:bg-slate-400"
             >
               Enviar
             </button>
